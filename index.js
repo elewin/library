f2ab86c22a53651c8f992eb49e348d25627ee0b7
Eric Lewin <Eric Lewin>
2015-12-05 22:49:01 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+//dependencies:
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//addresses and ports:
+var serverPort = 8080;
+var dbPort = 27017;
+var dbUri = 'mongodb://localhost:'+dbPort+'/library';
+
+mongoose.Promise = require('q').Promise; //replace mongo promises w/ q library
+app.use(express.static(__dirname+"/public"));
+app.use(bodyParser.json(), cors());
+
+//API endpoints:
+
+//server start-up:
+//connect to db
+mongoose.connect(dbUri, function(err){
+  if(err){
+    console.log('Unable to connect to '+dbUri);
+    console.log(err.message);
+  };
+});
+mongoose.connection.once('open', function(){
+  console.log('database connected to ' + dbUri);
+})
+//start server
+app.listen(serverPort, function(){
+  console.log('Library server listening to port '+serverPort);
+});
