649e689b1900ca1c4dd2b97b3d54db0577d3458a
elewin <elewin@gmail.com>
2015-12-18 15:18:28 -0600

    heroku!!!

--- config.js
+++ config.js
@@ -3,16 +3,16 @@ var config = require("./secrets.json"); // config file
 module.exports = {
   serverPort: process.env.PORT || config.ServerPort,
   mongodb : {
-    uri : config.mongodb.uri,
-    port:  config.mongodb.port,
-    db: config.mongodb.db,
+    uri : process.env.MONGOLAB_URI || config.mongodb.uri,
+    port: process.env.MONGOLAB_URI||  config.mongodb.port,
+    db: process.env.MONGOLAB_URI || config.mongodb.db,
   },
   mongolab : {
-    dbuser : config.mongolab.dbuser,
-    dbpassword: config.mongolab.dbpassword,
-    uriRoot :config.mongolab.uriRoot,
-    uri: config.mongolab.uri,
-    db: config.mongolab.db,
+    dbuser : process.env.MONGOLAB_URI || config.mongolab.dbuser,
+    dbpassword: process.env.MONGOLAB_URI || config.mongolab.dbpassword,
+    uriRoot : process.env.MONGOLAB_URI|| config.mongolab.uriRoot,
+    uri: process.env.MONGOLAB_URI|| config.mongolab.uri,
+    db: process.env.MONGOLAB_URI|| config.mongolab.db,
   },
   session: {
     secret: process.env.session || config.session.secret,
