23a7f030e8c36ca2b3c4194655ca78323348645d
Eric Lewin <Eric Lewin>
2015-12-30 22:45:13 -0600

    working on book view

--- public/js/controllers/bookPageCtrl.js
+++ public/js/controllers/bookPageCtrl.js
@@ -2,4 +2,10 @@ angular.module('library').controller('bookPageCtrl', function($scope, bookServic
 
   $scope.book = bookRef.data;
 
+  $scope.deleteBook = function(id){
+    bookService.deleteBook(id).then(function(res){
+      console.log('deleting book',id);
+    });
+  };
+
 });
